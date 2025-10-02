e320f3653b39a0a591084bea9323bf6ca07b5332
Yehor <omelianenkoegor@gmail.com>
2025-10-02 19:09:41 +0200

    tried doing auth failer miserably

--- src/components/appwrite.ts
+++ src/components/appwrite.ts
@@ -1,9 +1,10 @@
-import { Client, Databases } from 'appwrite';
+import { Client, Databases, Account } from 'appwrite';
 
 const client = new Client()
   .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
   .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
 export const databases = new Databases(client);
+export const account = new Account(client);
 export const databaseId = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 export const collectionId = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,9 +1,27 @@
-
-
+import { Slack } from "@/components/ui/svgs/slack";
+import { account } from "@/components/appwrite";
+import { OAuthProvider } from "appwrite";
 export default function LoginPage() {
+
+    function loginWSlack() {
+        account.createOAuth2Session({
+            provider: OAuthProvider.Slack,
+            success: window.location.origin,
+            failure: window.location.origin,
+            scopes: ["openid", "email", "profile"]
+        });
+    }
+
     return (
-        <div className="">
-            <h1>login</h1>
+        <div className="bg-accent rounded-xl border py-10 flex flex-col justify-center items-center">
+            <h1 className="font-mono text-2xl">Let's Login (alright?)</h1>
+            <span className="text-sm opacity-70">
+                You don't have a choice except for slack heheheh
+            </span>
+            <button onClick={loginWSlack} className="mt-4 grayscale hover:grayscale-0 flex gap-4 items-center bg-foreground p-2 rounded-lg px-5 text-card font-mono hover:scale-105 transition-all">
+                <Slack className="w-7" />
+                Log In with Slack
+            </button>
         </div>
-    )
+    );
 }
